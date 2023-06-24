8657610a2e3935f589cead152aefb6ccc8360d13
Smokey 8 <jacobramsdell316@gmail.com>
2023-06-24 16:49:12 -0700

    String Lab 4

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ def main():
     # Comlete your code here
     ##################################################
 
+    sub1 = (original_str[:6])
+    sub2 = (original_str.split()[1])
+    merged_str = (f"{sub2} {sub1}")
     print(sub2)
     print(sub1)
     print(merged_str)
-
+    
     #########################################
     # Do not delete the return statement
     return sub1, sub2, merged_str
